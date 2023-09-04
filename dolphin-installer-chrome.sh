e25ea01cfcc9b31ef4191d84557a421c697f99aa
Totsugeki64 <143974679+Totsugeki64@users.noreply.github.com>
2023-09-03 17:53:35 -0700

    dolphin installer for chromeos.
    this is just a way more convenient way to install dolphin on chrome os. just leave it running in the background and dolphin should open automatically once youre dont.

---
+++ dolphin-installer-chrome.sh
@@ -0,0 +1,40 @@
+echo "installing dolphin emulator for linux."
+echo "this is a remake of the offical way to install dolphin emulator (https://dolphin-emu.org/docs/guides/building-dolphin-linux/) made by Totsugeki"
+
+sudo apt install git
+
+sudo apt install --no-install-recommends ca-certificates qt6-base-dev qt6-base-private-dev libqt6svg6-dev git cmake make gcc g++ pkg-config libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libxi-dev libxrandr-dev libudev-dev libevdev-dev libsfml-dev libminiupnpc-dev libmbedtls-dev libcurl4-openssl-dev libhidapi-dev libsystemd-dev libbluetooth-dev libasound2-dev libpulse-dev libpugixml-dev libbz2-dev libzstd-dev liblzo2-dev libpng-dev libusb-1.0-0-dev gettext
+
+git clone https://github.com/dolphin-emu/dolphin.git dolphin-emu
+
+cd ./dolphin-emu
+
+git submodule update --init --recursive
+
+git checkout tags/5.0
+
+mkdir Build
+
+cd Build
+
+cmake .
+
+make -j$(nproc)
+
+sudo make install
+
+cmake -Dbindir=../Binary/Linux -Ddatadir=../Binary/Linux ..
+
+cd
+
+dolphin-emu
+
+clear
+
+echo "dolphin has been installed. The application will open automatically."
+
+echo "if dolphin-emu does not launch automatically, tpye "dolphin-emu" or look in your apps tab to launch it. It should be in your Linux apps.1"
+
+"if the installation failed, please make sure you have CMAKE, MAKE, and GIT installed, and try the installation again."
+
+echo "this is a remake of the offical way to install dolphin emulator. (https://dolphin-emu.org/docs/guides/building-dolphin-linux/) this code was made by Totsugeki"
